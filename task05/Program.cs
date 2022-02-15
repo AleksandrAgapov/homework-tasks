c1cc19500c9b8baeeeafb9f14e36d8e985f0e154
Alexander <alek.agapow80@yandex.ru>
2022-02-15 20:05:47 +0700

    task05

---
+++ task05/Program.cs
@@ -0,0 +1,18 @@
+﻿// 5.	Найти максимальное из трех чисел
+int a = 500;
+int b = 125;
+int c = 48;
+int max = 0;
+Console.Clear();
+{
+    if (a>b)
+    max =a;
+    else max=b;}
+    {
+if (c>b)
+    max = c;
+   
+    System.Console.WriteLine($"Максимальное число:{max}");
+    }
+  Console.ReadLine();
+    
