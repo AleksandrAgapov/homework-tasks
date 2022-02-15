2298a4a724a2c32eba490414c2723f46715217f0
Alexander <alek.agapow80@yandex.ru>
2022-02-15 20:13:51 +0700

    task07

---
+++ task07/Program.cs
@@ -0,0 +1,13 @@
+﻿// 7.	Выяснить является ли число чётным
+
+Console.WriteLine("Введите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num %2 == 0)
+
+System.Console.WriteLine($"Число {num} является четным");
+else System.Console.WriteLine($"Число {num} является  не четным");
+
+Console.ReadLine();
+
