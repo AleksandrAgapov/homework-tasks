2d69930f7f7300dd6e71ee25721f59021302d6bb
Alexander <alek.agapow80@yandex.ru>
2022-02-15 20:16:28 +0700

    task08

---
+++ task08/Program.cs
@@ -0,0 +1,14 @@
+﻿// 8.	Показать числа от -N до N
+
+ System.Console.WriteLine("введите отрицательное число");
+ int a  = Convert.ToInt32(Console.ReadLine());
+ 
+System.Console.WriteLine("введите положительное  число");
+ int b = Convert.ToInt32(Console.ReadLine());
+ 
+ while (a<b)
+ {
+    System.Console.WriteLine(a);
+     a++ ;
+ }
+
