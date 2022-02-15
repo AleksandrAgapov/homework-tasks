5ed99885f9762cdbbb02914fbe91aea40129a05b
Alexander <alek.agapow80@yandex.ru>
2022-02-15 20:21:23 +0700

    task09

---
+++ task09/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+ int a  = 1; 
+
+System.Console.WriteLine("введите максимальное положительное число N");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+ while (a<=b)
+ {
+     
+    System.Console.Write(a);
+     a++ ;
