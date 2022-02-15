2e34fb540301dfd23dc3d0d29e76770ff7aaf7d1
Alexander <alek.agapow80@yandex.ru>
2022-02-15 20:35:04 +0700

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,12 @@
+﻿// // Показать вторую цифру трёхзначного числа
+
+int a = 0; // задаем переменную
+string s = string.Empty; // зададим пустую строку
+
+System.Console.Write("Введите трехзначное число ");
+s = Console.ReadLine(); // положим введенное число в нашу строку
+a = s.Length; // подсчитаем длину строки
+
+
+if(a == 3) System.Console.WriteLine( "Вторая цифра трехзначного числа "  +s[s.Length^2]);
+else System.Console.WriteLine("Число не является трехзначным");
