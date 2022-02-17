10d1935d63e4181c80c8b67c32be3a266aaf429c
Alexander <alek.agapow80@yandex.ru>
2022-02-17 13:01:32 +0700

    task45

---
+++ task 45/Program.cs
@@ -0,0 +1,15 @@
+﻿// показать числа Фибоначи
+
+int []ArrayF = new int[15];
+ArrayF[0]=1;
+ArrayF[1]=1; 
+int sumArray;
+
+for( int i=2; i< ArrayF.Length; i++)
+{
+    ArrayF[i]=ArrayF[i-1]+ArrayF[i-2];
+   
+    System.Console.WriteLine("Число Фибоначи {0} равно {1}",i, ArrayF[i]);
+
+}
+ 
