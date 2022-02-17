c6ace4bf707726f13c7a0f1564a4b26fb8b2d475
Alexander <alek.agapow80@yandex.ru>
2022-02-17 13:20:05 +0700

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// 13. Удалить вторую цифру трехзначного сила
+
+int a, b, c,d,k;
+string s;
+ System.Console.WriteLine("введите трехзначное число");
+ s = Console.ReadLine();
+ a = Convert.ToInt32(s);
+ b= a%10;
+ c=b%10;
+ d=a/100;
+ k = (d*10)+b;
+ System.Console.WriteLine(k);
+
