973409dc02fd12f18a0472d309db957937a89ac2
Alexander <alek.agapow80@yandex.ru>
2022-02-19 17:12:23 +0700

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int x;
+int y;
+System.Console.WriteLine(" задайте кординату x  ");
+x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(" залайте координату y");
+y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 & y > 0)
+{
+    System.Console.WriteLine("Точка находится в I квадрате");
+}
+if (x < 0 & y > 0)
+{
+    System.Console.WriteLine("Точка находится в II квадрате");
+}
+if (x < 0 & y < 0)
+{
+    System.Console.WriteLine("Точка находится в III квадрате");
+}
+if (x > 0 & y < 0)
+{
+    System.Console.WriteLine("Точка находится в IV квадрате");
+}
+
