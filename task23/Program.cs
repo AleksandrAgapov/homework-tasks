04d96cc9276840b3d4df5c70df0f4be392a69751
Alexander <alek.agapow80@yandex.ru>
2022-02-21 20:09:56 +0700

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿//23.	Показать таблицу квадратов чисел от 1 до N
+
+
+Console.Clear();
+int a;
+string b;
+System.Console.WriteLine(" Введите число");
+b = Console.ReadLine();
+a = Convert.ToInt32(b);
+for (int j = 1; j <= a; j++)
+{
+    System.Console.Write(j * j + " ");
+}
