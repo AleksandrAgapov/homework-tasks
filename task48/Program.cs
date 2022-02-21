f8ea5f1bc7092f4a89e62a88fbe2dde38bb199a8
Alexander <alek.agapow80@yandex.ru>
2022-02-21 17:49:54 +0700

    task48

---
+++ task48/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//48.	Показать двумерный массив размером m×n заполненный целыми числами
+
+// int[,] b=new int[3,2]{{1,2},{3,4},{5,6}};
+
+// for(int i=0;i<3;i++)
+// for(int j=0;j<2;j++)
+// System.Console.WriteLine($"{i},{j}={b[i,j]}");
+ int[,] b=new int[2,2]{{1,2},{3,4}};
+void Print2D(int[,] b)
+{
+for(int i=0;i<b.GetLength(0);i++)
+{
+for(int j=0;j<b.GetLength(1);j++)
+System.Console.Write($"{b[i,j],4}");
+System.Console.WriteLine();
+}
+}
+
+Print2D(b);
