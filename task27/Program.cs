3054a51f459ad72312932a6627345c8585b53a52
Alexander <alek.agapow80@yandex.ru>
2022-02-22 14:02:55 +0700

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,9 @@
+﻿// 27.	Определить количество цифр в числе
+
+Console.Clear();
+int num;
+string b;
+System.Console.WriteLine(" Введите число  в котором считем цифры");
+b = Console.ReadLine();
+num = b.Length;
+System.Console.WriteLine("количество цифр в числе {0}", num);
