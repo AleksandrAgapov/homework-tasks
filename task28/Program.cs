bf47c011e1eab5b2c3c76c3916e6708f9cc591a4
Alexander <alek.agapow80@yandex.ru>
2022-02-22 14:23:46 +0700

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,18 @@
+﻿// 	28.	Подсчитать сумму цифр в числе
+
+Console.Clear();
+int num;
+int a = 0; // сумма цифр
+int c =0;  // отделяемая цифра 
+int b;    // переменная для остановки цикла 
+string d;
+System.Console.WriteLine(" Введите число  в котором считем сумму цифр");
+d = Console.ReadLine();
+num = Convert.ToInt32(d);
+while (num>0)
+{
+c=num%10;
+a=a+c;
+num=num/10;
+}
+System.Console.WriteLine("сумма цифр в веденном числе = {0}", a);
