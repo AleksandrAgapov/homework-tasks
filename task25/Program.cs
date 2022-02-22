e66d2f5735ba5716bcf93c3abb8a16ae660ce0d6
Alexander <alek.agapow80@yandex.ru>
2022-02-22 10:49:04 +0700

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿//25.	Найти сумму чисел от 1 до N
+
+
+
+Console.Clear();
+int n ;
+int i = 1;
+int sum = 0;
+string b;
+System.Console.WriteLine(" Введите число n");
+b = Console.ReadLine();
+n = Convert.ToInt32(b);
+while (i <= n)
+{
+    System.Console.WriteLine ( i );
+    sum = sum + i;
+    i++;
+
+}
+System.Console.WriteLine("Сумма чисел от 1 до {0} равна = {1} ", n, sum);
+
+
+
