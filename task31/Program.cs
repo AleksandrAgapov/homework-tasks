1b52363c129e8970aa177076b2c0bbc00ef35966
Alexander <alek.agapow80@yandex.ru>
2022-02-23 14:31:13 +0700

    task31

---
+++ task31/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//31.	Задать массив из 8 элементов и вывести их на экран
+Console.Clear();
+int[] array = new int[8];
+
+for(int i = 0; i<array.Length;i++)
+{
+     array[i]  = i;
+    System.Console.Write("|" +array[i]+"");
+    }
+    System.Console.WriteLine("|");
+           
+        
+    
+
