afc2a4c0e379d2c76cadacc31a69c67ec4a950b1
Alexander <alek.agapow80@yandex.ru>
2022-02-25 11:34:36 +0700

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,24 @@
+﻿//34.	Написать программу замену элементов массива на противоположные
+
+
+Console.Clear();
+int[] array = new int[10];
+int sumM = 0; 
+
+for (int i = 0; i <array.Length; i++)
+{
+    array[i] = new Random().Next(-5, 5);
+    System.Console.Write("|" + array[i] + "");
+    System.Console.Write("|");
+    
+}
+System.Console.WriteLine();
+
+for (int i = 0; i <array.Length; i++)
+{    
+     sumM = array[i]*(-1);
+    
+     System.Console.Write("|" + sumM + "");
+    System.Console.Write("|");
+    }
+    
