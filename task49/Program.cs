55bcfd6567abf114caa47add5bac0fce5e69167d
Alexander <alek.agapow80@yandex.ru>
2022-02-25 19:30:25 +0700

    task49

---
+++ task49/Program.cs
@@ -0,0 +1,20 @@
+﻿//49.	Показать двумерный массив размером m×n заполненный вещественными числами
+
+
+
+double[,]nums = new double[3,5]; // создали двумерный массив
+
+Random rand = new Random(); 
+
+for( int i=0; i<nums.GetLength(0);i++) //  перебор строк в массиве
+{   
+    for( int j=0; j<nums.GetLength(1);j++)     // перебор столбцов в массиве
+{     nums[i,j] = rand.NextDouble()*20;
+    nums[i,j] = rand.NextDouble()*20; //  присваивание значения элементу массива
+
+   
+    System.Console.Write(nums[i,j] +"\t"); // отображение элемента в строке
+}
+
+    System.Console.WriteLine(); // переход к новой строке
+}
