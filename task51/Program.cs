b5cc05a5346de139123ab05e9da7898a9c6bc2f9
Alexander <alek.agapow80@yandex.ru>
2022-02-26 17:34:34 +0700

    task51 внесли изменения

--- task51/Program.cs
+++ task51/Program.cs
@@ -1,34 +1,18 @@
 ﻿
 //51.	Задать двумерный массив следующим правилом: Aₘₙ = m+n
 
-int[,]nums = new int[5,5]; // создали двумерный массив
+int[,]nums = new int[5,5]; 
 
-for( int i=0; i<nums.GetLength(0);i++) //  перебор строк в массиве
+for( int i=0; i<nums.GetLength(0);i++) 
 {
-    for( int j=0; j<nums.GetLength(1);j++)     // перебор столбцов в массиве
+    for( int j=0; j<nums.GetLength(1);j++)     
 {
     nums[i,j]= i+j;
 
-        System.Console.Write($"{nums[i,j],6}|"); // отображение элемента в строке
+        System.Console.Write($"{nums[i,j],6}|"); 
 }
 
-    System.Console.WriteLine(); // переход к новой строке
+    System.Console.WriteLine(); 
 }
     System.Console.WriteLine();
 
-// for( int i=0; i<nums.GetLength(0);i++) //  перебор строк в массиве
-// {
-//     for( int j=0; j<nums.GetLength(1);j++)     // перебор столбцов в массиве
-// {
-//        if (nums[i,j]%2==0)
-    
-//     nums[i,j]=nums[i,j]*(-1);
-    
-//     else
-    
-//       nums[i,j]=nums[i,j];  
-    
-//     System.Console.Write($"{nums[i,j],6}|");
-//     }
-//     System.Console.WriteLine();
-// }
