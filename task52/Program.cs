62df467c6b2a718892999628a7560a14c96ffb45
Alexander <alek.agapow80@yandex.ru>
2022-02-28 13:43:19 +0700

    task52

---
+++ task52/Program.cs
@@ -0,0 +1,40 @@
+﻿
+//52.В двумерном массиве заменить элементы, у которых оба индекса чётные на  квадраты этих элементов
+
+int[,]nums = new int[5,5];
+int a =1; 
+
+for( int i=0; i<nums.GetLength(0);i++) 
+{
+    for( int j=0; j<nums.GetLength(1);j++)     
+{
+    nums[i,j]= a;
+    a++;
+        
+           System.Console.Write($"{nums[i,j],6}|"); 
+}
+
+    System.Console.WriteLine(); 
+}
+    System.Console.WriteLine();
+
+
+
+    for( int i=0; i<nums.GetLength(0);i++) 
+{
+    for( int j=0; j<nums.GetLength(1);j++)     
+{
+    //   nums[i,j]= a;
+    // a++;
+    if(i%2==0 && j%2==0)
+
+   nums[i,j]= (int) Math.Pow(nums[i,j],2);
+    
+
+    System.Console.Write($"{nums[i,j],6}|"); 
+}
+System.Console.WriteLine(); 
+}
+ System.Console.WriteLine();
+
+
